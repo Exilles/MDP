227c88f979a13bfb3ff2faa3dcce67763da35099
sq00465 <coldfear-dead@mail.ru>
2017-06-21 15:54:39 +0300

    Добавил регистрацию с заполнение таблицы 'users'

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'item'
 require 'item_store'
+require 'pg'
+require 'sequel'
 
+DB = Sequel.connect(:adapter=>'postgres', :host=>'localhost', :database=>'market', :user=>'admin', :password=>'111')
 store = ItemStore.new('item.yml')
 
 get('/inventory') do
@@ -10,13 +13,22 @@ get('/inventory') do
 end
 
 get('/inventory/new') do
-  erb :new
+    erb :new
+end
+
+get('/registration') do
+  erb :registration
+end
+
+post('/registration/accept') do
+  DB[:users].insert(:login => params['login'].to_s, :password => params['password'].to_s, :money => 100)
+  redirect '/inventory'
 end
 
 post('/inventory/create') do
   @item = Item.new
   @item.name = params['name']
-  #@item.count = params['count']
+  @item.count = params['count']
   @item.cost = params['cost']
   store.save(@item)
   redirect '/inventory/new'
